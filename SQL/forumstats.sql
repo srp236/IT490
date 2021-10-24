7e1ae938af204ddfe7e81b2d5a0468d7fea52766
md523 <60156213+md523@users.noreply.github.com>
2021-10-24 18:08:01 -0400

    table used to pull user forum stats

---
+++ SQL/forumstats.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS forumscore(
+   forumid int auto_increment, 
+   username VARCHAR(60),
+   score INT,
+   posts INT,
+   comments INT,
+   likes INT,
+   recipes INT,
+   PRIMARY KEY(forumid),
+   FOREIGN KEY(username) REFERENCES users(username)
+);
