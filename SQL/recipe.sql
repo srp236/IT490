34caca4b026c1a3afb1df7f63d568098f2c277b5
md523 <60156213+md523@users.noreply.github.com>
2021-11-01 20:10:04 -0400

    create table for recipes. initial query not run.

---
+++ SQL/recipe.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS recipe(
+    recipeID INT auto_increment, 
+    foodcatID INT, 
+    recipename VARCHAR,
+    recipedesc VARCHAR, 
+    preptime TIME,
+    cooketime TIME,
+    PRIMARY KEY(recipeID),
+    FOREIGN KEY(forcatID) REFERENCES FoodCat(foodcatID),
+);
