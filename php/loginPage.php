b803f2c0f9dfc2cbf450d0889b7a4c39ee7a1b61
mz342 <mz342@njit.edu>
2021-11-04 18:16:20 -0400

    added the sql part for the login

--- php/loginPage.php
+++ php/loginPage.php
@@ -54,7 +54,7 @@ if(isset($_POST["login"])){
 
     $dbPasswordHash = '';
     
-    
+
     
     if(password_verify($password, $dbPasswordHash)){
      echo "<br>You have succesfully logged in!<br>"; 
@@ -67,4 +67,54 @@ if(isset($_POST["login"])){
    echo "Validation issue found"; 
   }
 }
+  else
+		{
+			//the form has been posted without errors, so save it
+			//notice the use of mysql_real_escape_string, keep everything safe!
+			//also notice the sha1 function which hashes the password
+			$sql = "SELECT 
+						id,
+						username
+					FROM
+						Userstable
+					WHERE
+						username = '" . mysql_real_escape_string($_POST['username']) . "'
+					AND
+						password = '" . sha1($_POST['password']) . "'";
+						
+			$result = mysql_query($sql);
+			if(!$result)
+			{
+				//something went wrong, display the error
+				echo 'Something went wrong while signing in. Please try again later.';
+				//echo mysql_error(); //debugging purposes, uncomment when needed
+			}
+			else
+			{
+				//the query was successfully executed, there are 2 possibilities
+				//1. the query returned data, the user can be signed in
+				//2. the query returned an empty result set, the credentials were wrong
+				if(mysql_num_rows($result) == 0)
+				{
+					echo 'You have supplied a wrong user/password combination. Please try again.';
+				}
+				else
+				{
+					//set the $_SESSION['signed_in'] variable to TRUE
+					$_SESSION['signed_in'] = true;
+					
+					//we also put the user_id and user_name values in the $_SESSION, so we can use it at various pages
+					while($row = mysql_fetch_assoc($result))
+					{
+						$_SESSION['id'] 	= $row['id'];
+						$_SESSION['username'] 	= $row['username'];
+					}
+					
+					echo 'Welcome, ' . $_SESSION['username'] . '. <a href="index.php">Proceed to the forum overview</a>.';
+				}
+			}
+		}
+	
+
+
 ?>
