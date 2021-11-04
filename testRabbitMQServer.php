4c322a86865aa874d9b7592d77dcf7c137049021
shy <srp236@njit.edu>
2021-11-04 14:16:31 -0400

    test??

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -3,7 +3,7 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('login.php');
+require_once('login.php.inc');
 
 function doLogin($username,$password)
 {
