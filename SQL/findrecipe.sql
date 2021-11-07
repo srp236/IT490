367f698e7353bda23da59261594f7b93c13ca8ac
unknown <mz342@njit.edu>
2021-11-07 18:10:39 -0500

    adjusted again

--- SQL/findrecipe.sql
+++ SQL/findrecipe.sql
@@ -3,9 +3,7 @@ Select
     r.recipename AS rName,
     r.recipedesc AS rDesc,
     r.preptime AS pTime,
-    r.cuisine AS cuisine,
     r.cooktime AS cTime,
-    c.course_name AS course,
     f.foodcat AS fCat
   FROM recipe AS r
   JOIN foodcat AS fc ON fc.food_category_id = r.food_category_id
