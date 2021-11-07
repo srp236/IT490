b063507ec6096bac46a57c231fc8ebb933c1e87a
shy <srp236@njit.edu>
2021-11-06 23:20:52 -0400

    fixed

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -6,16 +6,15 @@ require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 
-$data = $_POST;
-
 $request = array();
-$request['type'] = "login";
-$request['username'] = $data['username'];
-$request['password'] = $data['password'];
+$request['type'] = $_POST['type'];
+$request['username'] = $_POSt['username'];
+$request['password'] = $_POST['password'];
 $request['message'] = "HI";
 $response = $client->send_request($request);
 //$response = $client->publish($request);
 
+echo json_encode($request);
 echo "client received response: ".PHP_EOL;
 print_r($response);
 echo "\n\n";
