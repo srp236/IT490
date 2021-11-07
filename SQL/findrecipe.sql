bb60ea074cef64fd6b5a736a7732b1676dacf7db
unknown <mz342@njit.edu>
2021-11-07 18:01:32 -0500

    adjusted findrecipe table

--- SQL/findrecipe.sql
+++ SQL/findrecipe.sql
@@ -1,9 +1,12 @@
 Select
-    r.recipeID AS rID
-    r.recipename AS rName
-    r.recipedesc AS rDesc
-    r.preptime AS pTime
-    r.cooktime AS cTime
+    r.recipeID AS rID,
+    r.recipename AS rName,
+    r.recipedesc AS rDesc,
+    r.preptime AS pTime,
+    r.cuisine AS cuisine,
+    r.cooktime AS cTime,
+    c.course_name AS course,
     f.foodcat AS fCat
   FROM recipe AS r
-  JOIN foodcat AS
+  JOIN foodcat AS fc ON fc.food_category_id = r.food_category_id
+ 
