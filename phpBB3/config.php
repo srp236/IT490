ba3d21f2272eef5afec3ecafd9f37adbcc8f6580
mz342 <mz342@njit.edu>
2021-11-08 12:35:37 -0500

    readding forum files

---
+++ phpBB3/config.php
@@ -0,0 +1,14 @@
+$dbms = 'phpbb\db\driver\mysqli';
+$dbhost = '172.29.245.74';
+$dbport = 'database port if not default';
+$dbname = 'IT490';
+$dbuser = 'shy';
+$dbpasswd = 'pass';
+$tableprefix = 'phbb';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\cache\driver\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
