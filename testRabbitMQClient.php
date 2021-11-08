6737d022141da3ec4a3866a36631e47c246d2228
shy <srp236@njit.edu>
2021-11-08 10:49:04 -0500

    just send return value

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -16,8 +16,8 @@ $request['message'] = "HI";
 $response = $client->send_request($request);
 //$response = $client->publish($request);
 
-echo "client received response:".PHP_EOL;
+#echo "client received response:".PHP_EOL;
 print_r($response);
-echo "\n\n";
-echo $argv[0]." END".PHP_EOL;
+#echo "\n\n";
+#echo $argv[0]." END".PHP_EOL;
 ?>
