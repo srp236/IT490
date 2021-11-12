13c6018d3e2ce44636c5e79aab8f166e66d9ed15
shy <srp236@njit.edu>
2021-11-11 21:46:13 -0500

    recipe table

--- SQL/recipe.sql
+++ SQL/recipe.sql
@@ -1,25 +1,13 @@
----------------------
---Table structure for `recipe`
----------------------
-
 USE `IT490`;
 
 DROP TABLE IF EXISTS`recipe`;
 
-CREATE TABLE `recipe` (
-    `recipeID` INT AUTO_INCREMENT,
-    `foodcatID` INT
-    ``
-
-
-)
-CREATE TABLE IF NOT EXISTS recipe(
-    recipeID INT auto_increment, 
-    foodcatID INT, 
-    recipename VARCHAR(48),
-    recipedesc VARCHAR(255), 
-    preptime TIME,
-    cooketime TIME,
-    PRIMARY KEY(recipeID),
-    FOREIGN KEY(foodcatID) REFERENCES foodcat(foodcatID)
+CREATE TABLE `recipe`(
+    `recipeid`    INT auto_increment, 
+    `dishname`    VARCHAR(20), 
+    `ptime`       VARCHAR(20),
+    `ctime`       VARCHAR(20), 
+    `ingred `     VARCHAR(255),
+    `instruc`     VARCHAR(255),
+    PRIMARY KEY(`recipeID`),
 );
